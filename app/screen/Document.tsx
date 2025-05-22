0a6f209799e46eb7fc06096f35785ad9c90a4bdc
Hrishikesh <hrishikesh.jadhav@anveshak.com>
2025-05-22 18:13:24 +0530

    added document picker with single image and pdf

--- app/screen/Document.tsx
+++ app/screen/Document.tsx
@@ -1,30 +1,100 @@
 import * as DocumentPicker from "expo-document-picker";
-import React, { useState } from "react";
-import { Button, Text, View } from "react-native";
+import * as FileSystem from "expo-file-system";
+import { useState } from "react";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import Pdf from "react-native-pdf";
+
 export default function Document() {
-  const [docData, setDocData] = useState({});
+  const [fileData, setFileData] = useState(null);
+  const [fileName, setFileName] = useState(null);
+  const testImg = /\.(jpeg|jpg|gif|png|webp|bmp|svg)$/i;
   const pickDocument = async () => {
     try {
       const result = await DocumentPicker.getDocumentAsync({
         type: "*/*",
         copyToCacheDirectory: true,
-        multiple: false,
       });
 
       if (result.canceled) {
-        console.log("User canceled document picking");
-      } else {
-        setDocData(result.assets[0]);
+        console.log("User cancelled");
+        return;
       }
+
+      const asset = result.assets[0];
+
+      const destPath = `${FileSystem.cacheDirectory}${asset.name}`;
+      await FileSystem.copyAsync({
+        from: asset.uri,
+        to: destPath,
+      });
+
+      setFileData(destPath);
+      setFileName(asset.name);
     } catch (err) {
       console.error("Document picking error:", err);
     }
   };
-  console.log(docData);
+  console.log(fileData);
   return (
-    <View>
-      <Text>Document</Text>
-      <Button title="Pick Document" onPress={pickDocument} />
+    <View style={{ flex: 1 }}>
+      {fileName && <Text style={styles.head}> {fileName}</Text>}
+      {fileData?.endsWith("pdf") ? (
+        <Pdf
+          source={{ uri: fileData }}
+          style={{ flex: 1 }}
+          onLoadComplete={(numPages) =>
+            console.log(`Loaded PDF with ${numPages} pages`)
+          }
+          onError={(error) => console.log("PDF error:", error)}
+        />
+      ) : fileData && testImg.test(fileData) ? (
+        <Image
+          style={styles.image}
+          source={{
+            uri: fileData,
+          }}
+        />
+      ) : (
+        <Image
+          style={styles.image}
+          source={require("../../assets/images/unknownfile.png")}
+        />
+      )}
+
+      <TouchableOpacity style={styles.btn} onPress={pickDocument}>
+        <Text style={styles.btnText}>Select file</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  image: {
+    width: 200,
+    height: 200,
+    marginHorizontal: "auto",
+    marginTop: 10,
+    borderRadius: 10,
+  },
+  head: {
+    textAlign: "center",
+    padding: 10,
+    fontSize: 16,
+    fontWeight: 700,
+  },
+  btn: {
+    justifyContent: "center",
+    position: "absolute",
+    width: "100%",
+    alignContent: "center",
+    alignItems: "center",
+    backgroundColor: "#000",
+    bottom: 0,
+  },
+  btnText: {
+    textAlign: "center",
+    padding: 10,
+    fontSize: 16,
+    color: "#fff",
+  },
+});
